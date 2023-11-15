5aa7ae9af7dff3a78bd2b30e926dc55632b59c2d
Rebas <reah24@bth.se>
2023-11-15 02:33:29 +0100

    create an object with properties and output the data in the console

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,1 +1,75 @@
-console.log("hello")
+
+const pokemonData = [
+    {
+      name: "Pikachu",
+      price: 25.99,
+      rating: 4.5,
+      category: "Electric",
+      image: "assets/img/pikachu.jpg", // Add the image file name or URL
+    },
+    {
+      name: "Charmander",
+      price: 49.99,
+      rating: 4.8,
+      category: "Fire/Flying",
+      image: "assets/img/charmander.jpg",
+    },
+    {
+      name: "Bulbasaur",
+      price: 19.99,
+      rating: 4.2,
+      category: "Grass/Poison",
+      image: "assets/img/bulbasaur.jpg",
+    },
+    {
+      name: "Squirtle",
+      price: 22.99,
+      rating: 4.3,
+      category: "Water",
+      image: "assets/img/squirtle.jpg",
+    },
+    {
+      name: "Graveler",
+      price: 18.99,
+      rating: 4.1,
+      category: "Rock",
+      image: "assets/img/graveler.jpg",
+    },
+    {
+      name: "Mewtwo",
+      price: 59.99,
+      rating: 4.9,
+      category: "Psychic",
+      image: "assets/img/mewtwo.jpg",
+    },
+    {
+      name: "Eevee",
+      price: 29.99,
+      rating: 4.6,
+      category: "Normal",
+      image: "assets/img/eevee.jpg",
+    },
+    {
+      name: "Jolteon",
+      price: 39.99,
+      rating: 4.7,
+      category: "Electric",
+      image: "assets/img/jolteon.jpg",
+    },
+    {
+      name: "Mew",
+      price: 69.99,
+      rating: 5.0,
+      category: "Psychic",
+      image: "assets/img/mew.jpg",
+    },
+    {
+      name: "Lugia",
+      price: 34.99,
+      rating: 4.4,
+      category: "Legendary",
+      image: "assets/img/lugia.jpg",
+    },
+  ];
+
+  console.log(pokemonData)
